41de6b411a7c795398e5ae9dac0b2dfde2dd1bfc
Jesus Parra <jhparra@hotmail.com>
2018-02-10 13:24:01 -0400

    agrege la funcion multiplicar

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,14 @@
-print ('Calculadora Suma y Resta')
+print ('Calculadora Suma, Resta y Multiplicacion ')
 numero_1 = int(input('Introduzca el numero 1 : '))
 numero_2 = int(input('Introduzca el numero 2 : '))
-operador = input('Quiere [S]umar o [R]estar : ')
+operador = input('Quiere [S]umar, [R]estar o [M]ultiplicar: ')
 if operador == 'S':
    valor = numero_1 + numero_2
    simbolo = ' + '
-else:
+elif operador == 'R':
    valor = numero_1 - numero_2
    simbolo = ' - '
+else:
+   valor = numero_1 * numero_2
+   simbolo = ' * '
 print (str(numero_1) + simbolo +  str(numero_2) + ' = ' + str(valor))
