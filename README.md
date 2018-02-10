0d0f095db1768d9a6a6ea0db4ebf13c844f99d8d
Jesus Parra <jhparra@hotmail.com>
2018-02-10 12:45:25 -0400

    agrege la funcion restar

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-El proposito de este pequeño proyecto, es con fines de aprender y profundizar 
-mis estudios en el lenguaje de python, ademas de usar la herramienta 
-Git y GiyHub. 
+# cal_python
+Barquisimeto 10-02-2018
+
+El proposito de este pequeño proyecto, es con fines de aprender y profundizar mis estudios en el lenguaje de python, ademas de usar la herramienta Git y GiyHub. 
--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,11 @@
-print ('Calculadora Suma')
+print ('Calculadora Suma y Resta')
 numero_1 = int(input('Introduzca el numero 1 : '))
 numero_2 = int(input('Introduzca el numero 2 : '))
-valor = numero_1 + numero_2
-print ('La suma de ' + str(numero_1) + ' + ' + str(numero_2) + ' es ' + str(valor))
+operador = input('Quiere [S]umar o [R]estar : ')
+if operador == 'S':
+   valor = numero_1 + numero_2
+   simbolo = ' + '
+else:
+   valor = numero_1 - numero_2
+   simbolo = ' - '
+print (str(numero_1) + simbolo +  str(numero_2) + ' = ' + str(valor))
