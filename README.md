3d85bce3ad00f4a3d537e5a9b5e98420d3d1a20f
Jesus Parra <jhparra@hotmail.com>
2018-02-10 12:06:59 -0400

    Inicio de mi calculadora en python

---
+++ README.md
@@ -0,0 +1,3 @@
+El proposito de este pequeño proyecto, es con fines de aprender y profundizar 
+mis estudios en el lenguaje de python, ademas de usar la herramienta 
+Git y GiyHub. 
---
+++ calculadora.py
@@ -0,0 +1,5 @@
+print ('Calculadora Suma')
+numero_1 = int(input('Introduzca el numero 1 : '))
+numero_2 = int(input('Introduzca el numero 2 : '))
+valor = numero_1 + numero_2
+print ('La suma de ' + str(numero_1) + ' + ' + str(numero_2) + ' es ' + str(valor))
