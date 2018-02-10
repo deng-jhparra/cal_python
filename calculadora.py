c3c203c85733f2733102f028c4e0b211fcec632d
Jesus Parra <jhparra@hotmail.com>
2018-02-10 17:58:19 -0400

    Agrego funcion dividir

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,17 @@
 print ('Calculadora Suma, Resta y Multiplicacion ')
 numero_1 = int(input('Introduzca el numero 1 : '))
 numero_2 = int(input('Introduzca el numero 2 : '))
-operador = input('Quiere [S]umar, [R]estar o [M]ultiplicar: ')
+operador = input('Quiere [S]umar, [R]estar, [M]ultiplicar o [D]ividir: ')
 if operador == 'S':
    valor = numero_1 + numero_2
    simbolo = ' + '
 elif operador == 'R':
    valor = numero_1 - numero_2
    simbolo = ' - '
-else:
+elif operador == 'M':
    valor = numero_1 * numero_2
    simbolo = ' * '
+else:
+   valor = numero_1 / numero_2
+   simbolo = ' / '
 print (str(numero_1) + simbolo +  str(numero_2) + ' = ' + str(valor))
